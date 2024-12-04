33bbf3fc2c686aad5b75c266b024bb03a97adc32
apakalypse <80667958+apakalypse@users.noreply.github.com>
2024-12-04 11:50:42 -0500

    Add scraper module

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -1,5 +1,5 @@
-from .pubmed import PubMedScraper
-from .arxiv import ArxivScraper
-from .researchgate import ResearchGateScraper
-
-__all__ = ['PubMedScraper', 'ArxivScraper', 'ResearchGateScraper']
+{
+  "message": "Add scraper module",
+  "content": "ZnJvbSAucHVibWVkIGltcG9ydCBQdWJNZWRTY3JhcGVyCmZyb20gLmFyeGl2IGltcG9ydCBBcnhpdlNjcmFwZXIKZnJvbSAucmVzZWFyY2hnYXRlIGltcG9ydCBSZXNlYXJjaEdhdGVTY3JhcGVyCgpfX2FsbF9fID0gWydQdWJNZWRTY3JhcGVyJywgJ0FyeGl2U2NyYXBlcicsICdSZXNlYXJjaEdhdGVTY3JhcGVyJ10=",
+  "encoding": "base64"
+}
